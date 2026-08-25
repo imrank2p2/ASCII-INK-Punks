ec6d07fe4f56f5cee4c9f256c9299557a7139372
imrank2p2 <imran16031985@gmail.com>
2026-08-25 23:59:01 +0530

    Update page.tsx

--- app/vault/page.tsx
+++ app/vault/page.tsx
@@ -28,7 +28,7 @@ declare global {
     ethereum?: {
       request: (args: { method: string; params?: unknown[] }) => Promise<unknown>;
       on?: (event: string, listener: (...args: unknown[]) => void) => void;
-      removeListener?: (event: string, listen<imgPreloader key={src} src={src} />er: (...args: unknown[]) => void) => void;
+     removeListener?: (event: string, listener: (...args: unknown[]) => void) => void;
     };
   }
 }
